2cc685f0309634d900d0b821e12737ef0201d26f
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2019-11-26 00:40:12 +0000

    feat(storyController): Experimentation on testing express middleware controllers

--- src/collections/Story/controller/storyController.spec.ts
+++ src/collections/Story/controller/storyController.spec.ts
@@ -2,30 +2,49 @@ import { expect } from 'chai';
 import 'mocha';
 import { StoryController } from './storyController';
 import sinon from 'sinon';
-import { IReq, IRes } from '../../../utilities/interfaces/IMiddlewareParams';
+import {
+  IReq,
+  IRes,
+  INext
+} from '../../../utilities/interfaces/IMiddlewareParams';
 import { Story } from '../model/storyEntity';
 import chai from 'chai';
 import sinonChai from 'sinon-chai';
+import { mockResponse, mockRequest } from 'mock-req-res';
+import { QueryService } from '../../../services/query/queryService';
 
 chai.use(sinonChai);
 
 describe('storyController', () => {
-  const controller = new StoryController();
-  const model = new Story();
+  let res: any = mockResponse(),
+    req: IReq = mockRequest(),
+    next: INext = sinon.spy(),
+    controller: StoryController = new StoryController();
 
   describe('when requesting for all stories', () => {
-    const req: Partial<IReq> = {
-      body: {
-        user: '0000'
-      }
-    };
-    const res = {};
-    const next = sinon.stub();
+    sinon.spy(controller['_queryService'], 'buildParamQuery');
+    sinon.spy(controller['_modelDb'], 'findMany');
 
     beforeEach(() => {
-      controller.getAll(req as IReq, res as IRes, next);
+      req.body = {
+        user: '0000'
+      };
+      req.params = { storyId: '0000' };
+      controller.getAll(req, res, next);
     });
 
-    it('should call the validate function');
+    it('should build the parameter query', () => {
+      expect(
+        controller['_queryService'].buildParamQuery
+      ).to.have.been.calledWith(req.params);
+    });
+
+    it('should make a call to the database service', () => {
+      const result = controller['_queryService'].buildParamQuery(req.params);
+      expect(controller['_modelDb'].findMany).to.have.been.calledWith(
+        '',
+        result
+      );
+    });
   });
 });
